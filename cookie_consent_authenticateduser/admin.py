7dcefd9e783620eb46daa64e19cadd5131a0143d
Thomas <thomas.carbillet@gmail.com>
2021-04-08 11:15:59 +0200

    feat: add user__email serach field

--- cookie_consent_authenticateduser/admin.py
+++ cookie_consent_authenticateduser/admin.py
@@ -7,6 +7,7 @@ class AuthenticatedUserCookieConsentAdmin(admin.ModelAdmin):
     list_display = ("user", "action", "cookiegroup")
     list_filter = ("cookiegroup",)
     raw_id_fields = ("user",)
+    search_fields = ("user__email",)
 
 
 admin.site.register(AuthenticatedUserCookieConsent, AuthenticatedUserCookieConsentAdmin)
